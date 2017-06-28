8f0a1c249685c1bc2c327718bc7c0c915849c25c
pittbull <pbull809@gmail.com>
2017-06-28 14:21:26 +0200

    webserver changed to ajax

--- src/jforth/Utilities.java
+++ src/jforth/Utilities.java
@@ -18,8 +18,8 @@ import java.util.List;
  */
 public class Utilities
 {
-    public static final String BUILD_NUMBER = "643";
-    public static final String BUILD_DATE = "06/28/2017 11:33:46 AM";
+    public static final String BUILD_NUMBER = "674";
+    public static final String BUILD_DATE = "06/28/2017 02:15:10 PM";
 
     public static final String buildInfo = "JForth, Build: " + Utilities.BUILD_NUMBER + ", " + Utilities.BUILD_DATE;
 
--- src/webserver/SimpleWebserver.java
+++ src/webserver/SimpleWebserver.java
@@ -3,7 +3,6 @@ package webserver;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 import jforth.JForth;
-import jforth.Utilities;
 import tools.StringStream;
 
 import java.io.IOException;
@@ -14,29 +13,6 @@ import java.net.URLDecoder;
 
 public class SimpleWebserver
 {
-    static private final String form = "<form action=\"\" method=\"GET\">\n" +
-            "<font color=\"gray\">Type commands here and press return</font> </br>" +
-            "<input type=\"text\" id=\"InputID\"  name=\"msg\" value=\"\" size=\"50\">\n" +
-            "</form>";
-    static private final String pageStart = "<html>" +
-            "<head>\n<meta charset=\"utf-8\"/>" +
-            "<style>\n" +
-            "\n" +
-            "      p { margin-left: 3em; }\n" +
-            "\n" +
-            "</style>" +
-            "<script type=\"text/javascript\">\n" +
-            "function FocusOnInput()\n" +
-            "{\n" +
-            "     document.getElementById(\"InputID\").focus();\n" +
-            "}\n" +
-            "</script>" +
-            "<script src=\"jquery.js\"></script>" +
-            "</head><body bgcolor=\"#000000\" onload=\"FocusOnInput()\">" +
-            "<font color=\"gray\">" + Utilities.buildInfo + "</font>" +
-            "<hr><code>";
-    static private final String pageEnd = "</code><hr>" + form + "</body></html>";
-
     public static void start (int port)
     {
         new Thread(() ->
@@ -52,6 +28,14 @@ public class SimpleWebserver
         }).start();
     }
 
+    private static void sendContent (String name, OutputStream os) throws IOException
+    {
+        InputStream jqStream = ClassLoader.getSystemResourceAsStream(name);
+        byte[] buff = new byte[jqStream.available()];
+        jqStream.read (buff);
+        os.write(buff);
+    }
+
     private static void startServer (int port) throws IOException
     {
         StringStream _ss = new StringStream();
@@ -61,43 +45,36 @@ public class SimpleWebserver
 
         HttpHandler httpHandler = e ->
         {
-            System.out.println("HTTP: " + e.getRequestURI().toString());
-            String[] txt = e.getRequestURI().toString().split("=");
+            //System.out.println("HTTP: " + e.getRequestURI().toString());
+            String txt = e.getRequestURI().toString();
+            String[] split = txt.split("\\?");
             OutputStream os = e.getResponseBody();
             e.sendResponseHeaders(200, 0);
-            if (txt[0].equals("/?msg"))
+            if (txt.equals("/"))
             {
-                String cmd = URLDecoder.decode(txt[1], "UTF-8");
+                sendContent("page.html", os);
+            }
+            else if (split[0].equals("/forth"))
+            {
+                String cmd = URLDecoder.decode(split[1],"UTF-8");
                 forth.singleShot(cmd);
-                String erg = _ss.toString().replace("\n", "<br>");
-                os.write(makePage(cmd + "<br>" + erg.substring(0, erg.length() - 2)));
+                String erg = _ss.toString();
+                os.write (cmd.getBytes());
+                os.write ("\n".getBytes());
+                os.write(erg.getBytes(), 0, erg.length()-2);
                 _ss.clear();
             }
-            else if (txt[0].equals("/jquery.js"))
+            else if (txt.equals("/jquery.js")) //send jquery
             {
-                InputStream jqStream =
-                        ClassLoader.getSystemResourceAsStream("jquery-3.2.1.min.js");
-                byte[] buff = new byte[jqStream.available()];
-                jqStream.read (buff);
-                os.write(buff);
+                sendContent("jquery-3.2.1.min.js", os);
             }
-            else
+            else if (txt.equals("/favicon.ico")) //send jquery
             {
-                os.write(makePage("oops?"));
+                sendContent("tiger.png", os);
             }
             os.close();
         };
-
         server.createContext("/", httpHandler);
         server.start();
-    }
-
-    private static byte[] makePage (String content)
-    {
-        final String sb = pageStart +
-                        "<p><font color=\"yellow\">" + content + "</font></p>" +
-                        pageEnd;
-
-        return sb.getBytes();
     }
 }
