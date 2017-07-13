94374a7e70ca48ae85cc00d7d4a7affaadcd3238
pittbull <pbull809@gmail.com>
2017-07-13 11:07:30 +0200

    added polynomial fitting and poly rounding

--- src/jforth/Utilities.java
+++ src/jforth/Utilities.java
@@ -18,8 +18,8 @@ import java.util.List;
  */
 public class Utilities
 {
-    public static final String BUILD_NUMBER = "694";
-    public static final String BUILD_DATE = "07/13/2017 11:00:12 AM";
+    public static final String BUILD_NUMBER = "695";
+    public static final String BUILD_DATE = "07/13/2017 11:03:20 AM";
 
     public static final String buildInfo = "JForth, Build: " + Utilities.BUILD_NUMBER + ", " + Utilities.BUILD_DATE;
 
