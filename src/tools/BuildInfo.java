01cb87aafb6c735b59428847e7873772f1f599ea
Administrator <pbull809@gmail.com>
2022-05-31 12:29:55 +0200

    added missing library zxing-2.1.jar

--- src/tools/BuildInfo.java
+++ src/tools/BuildInfo.java
@@ -1,8 +1,8 @@
 package tools;
 
 public class BuildInfo {
-    private static final String BUILD_NUMBER = "3075";
-    private static final String BUILD_DATE = "05/31/2022 12:15:22 PM";
+    private static final String BUILD_NUMBER = "3077";
+    private static final String BUILD_DATE = "05/31/2022 12:27:24 PM";
 
     public static final String buildInfo = "JForth, Build: " + BUILD_NUMBER + ", " + BUILD_DATE
             + " -- " + System.getProperty("java.version");
