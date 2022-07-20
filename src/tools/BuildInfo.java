7bd0901a50770cf7c7106cf110f9144026923438
Administrator <pbull809@gmail.com>
2022-07-20 21:57:35 +0200

    created new jarfile

--- src/tools/BuildInfo.java
+++ src/tools/BuildInfo.java
@@ -1,8 +1,8 @@
 package tools;
 
 public class BuildInfo {
-    private static final String BUILD_NUMBER = "3315";
-    private static final String BUILD_DATE = "07/20/2022 02:01:44 PM";
+    private static final String BUILD_NUMBER = "3318";
+    private static final String BUILD_DATE = "07/20/2022 09:55:53 PM";
 
     public static final String buildInfo = "JForth, Build: " + BUILD_NUMBER + ", " + BUILD_DATE
             + " -- " + System.getProperty("java.version");
