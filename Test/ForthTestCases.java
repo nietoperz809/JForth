d92066be7a411c6aeef71d11a4925b8dbebeda3b
Administrator <pbull809@gmail.com>
2023-10-25 02:07:16 +0200

    added test for variable increment & decrement

--- Test/ForthTestCases.java
+++ Test/ForthTestCases.java
@@ -1846,4 +1846,16 @@ public class ForthTestCases extends TestBase
         shouldBeThis("{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20}" ,s);
     }
 
+    @Test
+    public void TestVarIncDec()
+    {
+        String s = check ("variable fuck 33 fuck ! fuck ++! fuck @",".");
+        System.out.println(s);
+        shouldBeThis("34" ,s);
+        s = check ("variable fuck 33 fuck ! fuck --! fuck @",".");
+        System.out.println(s);
+        shouldBeThis("32" ,s);
+    }
+
+
 }
